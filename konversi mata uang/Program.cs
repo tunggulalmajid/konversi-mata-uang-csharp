b123af2542f0488bd8fc144596ef19187d5aabcf
tunggulalmajid <tunggulmajid@gmail.com>
2025-02-26 20:31:38 +0700

    Add project files.

---
+++ konversi mata uang/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konversi_mata_uang
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            header();
+            Console.WriteLine("konversi mata uang dari :");
+            Console.WriteLine($"1. Dolar\n" + $"2. Rupiah" );
+            garis();
+            try
+            {
+                Console.Write("masukkan pilihan >> ");
+                int pilihan = Convert.ToInt32(Console.ReadLine());
+                if (pilihan == 1)
+                {
+                    convertDolar();
+                }
+                else if (pilihan == 2)
+                {
+
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("inputan tidak valid");
+            }
+            void garis() 
+            {
+                Console.WriteLine("============================================================");
+            }
+            void header()
+            {
+                garis();
+                Console.WriteLine("Koverter Mata Uang".PadLeft(37));
+                garis();
+            }
+            void convertDolar()
+            {
+                Console.Clear();
+                header();
+                Dollar dolar = new Dollar();
+                Console.Write("masukkan nilai uang yang iongin dikonversikan >> ");
+                dolar.Nilai = Convert.ToDouble(Console.ReadLine());
+                dolar.tampilkanHasil();
+                garis();
+                Console.WriteLine("apakah ingin convert lagi [y] or [n] >> ");
+            } 
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ konversi mata uang/dollar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konversi_mata_uang
+{
+    internal class Dollar
+    {
+        private double nilai;
+        public double Nilai 
+        { 
+            get { return nilai; }
+            set { nilai = value; } 
+        }
+        public double convertToRupiah()
+        {
+            double hasilConvertRupiah = nilai * 15000;
+            return hasilConvertRupiah;
+        }
+        public void tampilkanHasil()
+        {
+            double hasil = convertToRupiah();
+            Console.WriteLine($"Dollar >> ${nilai}");
+            Console.WriteLine($"Rupiah >> Rp.{hasil}");
+        }
+    }
+}
